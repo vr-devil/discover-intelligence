bb71fcb2d39936440df46a68f500a7636566913f
Kai Zhong <cnaicc@gmail.com>
2019-07-24 17:50:55 +0800

    Version 20190724 - rev 1

--- cnn_practice/keras_model.py
+++ cnn_practice/keras_model.py
@@ -11,17 +11,26 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 x_train = np.reshape(x_train, (60000, 28, 28, 1))
 x_test = np.reshape(x_test, (10000, 28, 28, 1))
 
+# x_train, y_train = x_train[:1000], y_train[:1000]
+# x_test, y_test = x_test[:1000], y_test[:1000]
+
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
 model = tf.keras.models.Sequential([
     layers.Conv2D(64, (2, 2), activation='relu', input_shape=(28, 28, 1)),
     layers.MaxPool2D((2, 2)),
+    layers.Conv2D(128, (2, 2), activation='relu'),
+    layers.MaxPool2D((2, 2)),
     layers.Flatten(),
     layers.Dropout(0.4),
-    layers.Dense(128, activation='sigmoid'),
-    layers.Dense(10, activation='sigmoid')
+    layers.Dense(256, activation='relu'),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer='SGD', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=30)
+model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=15)
 model.evaluate(x_test, y_test)
 
-# test accuracy: 0.8942
+# test accuracy: 0.9018
