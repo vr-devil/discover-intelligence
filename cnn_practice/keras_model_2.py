2873d6b7e99cd76b8fbd4e3cf969381f6a9cb8ba
Kai Zhong <cnaicc@gmail.com>
2019-07-26 20:06:14 +0800

    Version 20190726

---
+++ cnn_practice/keras_model_2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+x_train = np.reshape(x_train, (60000, 28, 28, 1))
+x_test = np.reshape(x_test, (10000, 28, 28, 1))
+
+# x_train, y_train = x_train[:10000], y_train[:10000]
+x_val, y_val = x_test[:1000], y_test[:1000]
+
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+y_val = tf.keras.utils.to_categorical(y_val, 10)
+
+model = tf.keras.models.Sequential([
+    layers.Conv2D(64, (2, 2), activation='relu', input_shape=(28, 28, 1)),
+    layers.MaxPool2D((2, 2)),
+    layers.BatchNormalization(),
+    layers.Conv2D(128, (2, 2), activation='relu'),
+    layers.MaxPool2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(500, activation='relu'),
+    layers.Dense(1000, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=30, validation_data=(x_test, y_test))
+model.evaluate(x_test, y_test)
+
+# test accuracy: 0.9207
